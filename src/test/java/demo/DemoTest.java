bd4fa4e11671986bc765b5b5a118d6f9d53302fd
Yash Sanghai <Yash Sanghai@DESKTOP-APNSSC1>
2022-11-19 11:45:03 +0530

    Commit 1- DemoTest

---
+++ src/test/java/demo/DemoTest.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test
+	public  void demo() {
+		
+		System.out.println(" This is is a WASM6 project");
+
+	}
+
+}
